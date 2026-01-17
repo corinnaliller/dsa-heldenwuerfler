44cab48301cf19339682b4db49618ebf095849e4
corinnaliller <corinna.liller@gmx.net>
2026-01-17 08:16:12 +0100

    Add Char class

---
+++ Heldenwuerfler/Logik/Char.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Heldenwuerfler.Logik
+
+public class Char
+{
+    #region Properties
+    public string Name { get; set; }
+    public int LeP { get; set; }
+    public List<Eigenschaft> Eigenschaften { get; set; }
+    #endregion Properties
+
+    #region Constructors
+    public Char()
+	{
+        Name = "Menschenjäger/Gardist";
+        LeP = 32;
+        Eigenschaften = new List<Eigenschaft>();
+    }
+    #endregion Constructors
+}
