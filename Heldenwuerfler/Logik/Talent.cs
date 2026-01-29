b48d33b6aedeff265b1a70ae3faf21300b8ad2b4
corinnaliller <corinna.liller@gmx.net>
2026-01-29 16:11:58 +0100

    Talent-Klasse hinzugefügt

---
+++ Heldenwuerfler/Logik/Talent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heldenwuerfler.Logik
+{
+    internal class Talent
+    {
+        #region Properties
+        internal TalentKategorie Kategorie { get; set; }
+        internal string Name { get; set; }
+        internal int Wert { get; set; }
+        internal List<Eigenschaft> Probe { get; set; }
+        internal bool Basis { get; private set; }
+        #endregion Properties
+
+        #region Constructors
+        public Talent() { }
+        public Talent(string name, int wert, List<Eigenschaft> probe, TalentKategorie kategorie, bool basis)
+        {
+            Name = name;
+            Wert = wert;
+            Probe = probe;
+            Basis = basis;
+            Kategorie = kategorie;
+        }
+        #endregion Constructors
+    }
+}
