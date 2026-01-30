b6274aeaefdbaf98bd787c0ac5b7cc5211a0b5b4
corinnaliller <corinna.liller@gmx.net>
2026-01-30 18:14:01 +0100

    Talent bearbeiten

--- Heldenwuerfler/Logik/Talent.cs
+++ Heldenwuerfler/Logik/Talent.cs
@@ -12,13 +12,13 @@ namespace Heldenwuerfler.Logik
         internal TalentKategorie Kategorie { get; set; }
         internal string Name { get; set; }
         internal int Wert { get; set; }
-        internal List<Eigenschaft> Probe { get; set; }
+        internal List<string> Probe { get; set; }
         internal bool Basis { get; private set; }
         #endregion Properties
 
         #region Constructors
         public Talent() { }
-        public Talent(string name, int wert, List<Eigenschaft> probe, TalentKategorie kategorie, bool basis)
+        public Talent(string name, int wert, List<string> probe, TalentKategorie kategorie, bool basis)
         {
             Name = name;
             Wert = wert;
@@ -26,6 +26,38 @@ namespace Heldenwuerfler.Logik
             Basis = basis;
             Kategorie = kategorie;
         }
+        public Talent(string name, string wert, string probe, string kategorie)
+        {
+            Name = name;
+            if (!Int32.TryParse(wert, out Wert))
+            {
+                Wert = 0;
+            }
+            Probe = ParseEigenschaften(probe);
+            Kategorie = KategorieSetzen(kategorie);
+        }
         #endregion Constructors
+
+        #region Private Methods
+        private List<string> ParseEigenschaften(string eigenschaften)
+        {
+            string[] split = eigenschaften.Split("/");
+            List<string> liste = new List<string>();
+            foreach (string s in split)
+            {
+                liste.Add(s);
+            }
+            return liste;
+        }
+        private TalentKategorie KategorieSetzen(string kategorieName)
+        {
+            switch (kategorieName) {
+                case "Körperlich": return TalentKategorie.Koerperlich;
+                case "Gesellschaft": return TalentKategorie.Gesellschaft;
+                case "Natur": return TalentKategorie.Natur;
+                case "Wissen": return TalentKategorie.Wissen;
+                case "Handwerk": return TalentKategorie.Handwerk;
+            }
+        #endregion Private Methods
     }
 }
